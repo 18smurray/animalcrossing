19b84845b4dc8230756234d44eb26144bffece6d
18smurray <66044394+18smurray@users.noreply.github.com>
2022-04-13 14:49:34 -0600

    Fixed roster render

--- src/VillageRoster.js
+++ src/VillageRoster.js
@@ -18,19 +18,16 @@ function VillageRoster(props) {
           <h2>Villager Roster</h2>
           <div className='villagerList'>
             {/* For each villager in the roster, create an info card with a button to remove and to get more info */}
-            {roster.map(villager => {
+            {roster.map(villager => (
                 <div className="villager" key={villager.id}>
                     <img src={villager.image_uri} className="profilePic"></img>
                     <h3 className='villagerName'>{villager.name}</h3>
                     <Link to={`villager/${villager.id}`} key={villager.id}>
-                      <button className='moreInfo' onClick={() => {
-                        window.scrollTo({top: 0, behavior: 'smooth'}); 
-                        document.activeElement.blur();
-                        }}>INFO</button>
+                        <button className='moreInfo' onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'}); document.activeElement.blur()}}>INFO</button>
                     </Link>
                     <button className='remove' onClick={() => evictVillager(villager.id)}>&times;</button>
                 </div>
-            })}
+            ))}
           </div>
         </section> 
     )
